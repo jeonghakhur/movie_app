05e02960bac6bec73b0fdd86c2c8d985de07de09
jeonghak <jeonghak.jur@gmail.com>
2018-12-12 18:59:06 +0900

    functional component

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
           }
         ]
       })
-    }, 5000)
+    }, 2000)
   }
 
   _renderMovies = () => {
--- src/Movie.js
+++ src/Movie.js
@@ -1,33 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  }
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    )
-  }
+function Movie({title, poster}) {
+  return (
+    <div>
+      <MoviePoster poster={poster} title={title} />
+      <h1>{title}</h1>
+    </div>
+  )
 }
 
-class MoviePoster extends Component {
+function MoviePoster({poster, title}) {
+  return (
+    <img src={poster} alt={title} />
+  )
+}
 
-  static propTypes = {
-    poster: PropTypes.string.isRequired
-  }
-  render() {
-    return (
-      <img src={this.props.poster} alt="" />
-    )
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
 
 export default Movie;
