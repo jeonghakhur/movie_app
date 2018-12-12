91bd613f0c340cc9f961905637bc77ebfd3692d6
jeonghak <jeonghak.jur@gmail.com>
2018-12-12 14:20:43 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import React, {Component} from 'react';
 import Movie from './Movie';
 import './App.css';
 
-const movies = [
-  {
-    title: "Avengers",
-    image: "https://yts.am/assets/images/movies/avengers_infinity_war_2018/large-cover.jpg"
-  }, {
-    title: "Black Panther",
-    image: "https://yts.am/assets/images/movies/black_panther_2018/large-cover.jpg"
-  }, {
-    title: "Deadpool 2",
-    image: "https://yts.am/assets/images/movies/deadpool_2_2018/large-cover.jpg"
-  }, {
-    title: "Big Hero 6",
-    image: "https://yts.am/assets/images/movies/big_hero_6_2014/large-cover.jpg"
-  }
-]
-
 class App extends Component {
 
   // Render:
@@ -33,22 +17,41 @@ class App extends Component {
   // componentDidUpdate
 
   state = {
-    greeting: 'Hello!'
+    movies: [
+      {
+        title: "Avengers",
+        image: "https://yts.am/assets/images/movies/avengers_infinity_war_2018/large-cover.jpg"
+      }, {
+        title: "Black Panther",
+        image: "https://yts.am/assets/images/movies/black_panther_2018/large-cover.jpg"
+      }, {
+        title: "Deadpool 2",
+        image: "https://yts.am/assets/images/movies/deadpool_2_2018/large-cover.jpg"
+      }, {
+        title: "Big Hero 6",
+        image: "https://yts.am/assets/images/movies/big_hero_6_2014/large-cover.jpg"
+      }
+    ]
   }
 
   componentDidMount() {
     setTimeout(() => {
       // this.state.greeting = 'something';
       this.setState({
-       greeting: 'Hello again!' 
+        movies: [
+          {
+            title: "Thor: Ragnarok",
+            image: "https://yts.am/assets/images/movies/thor_ragnarok_2017/large-cover.jpg"
+          },
+          ...this.state.movies
+        ]
       })
     }, 5000)
   }
   render() {
-    console.log(this.state.greeting);
     return (
       <div className="App">
-        {movies.map((movie, index) => 
+        {this.state.movies.map((movie, index) => 
            <Movie 
             title={movie.title}
             poster={movie.image}
