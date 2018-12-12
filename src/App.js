caab75d8e7c109205d080b1f25bfccbbded848b3
jeonghak <jeonghak.jur@gmail.com>
2018-12-12 13:17:25 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Movie from './Movie';
 import './App.css';
 
-const movie_title = [
-  "Avengers",
-  "Black Panther",
-  "Deadpool 2",
-  "Big Hero 6"
-]
+const movie_title = ["Avengers", "Black Panther", "Deadpool 2", "Big Hero 6"]
 
-const movie_images = [
-  "https://yts.am/assets/images/movies/avengers_infinity_war_2018/large-cover.jpg",
-  "https://yts.am/assets/images/movies/black_panther_2018/large-cover.jpg",
-  "https://yts.am/assets/images/movies/deadpool_2_2018/large-cover.jpg",
-  "https://yts.am/assets/images/movies/big_hero_6_2014/large-cover.jpg"
+const movie_images = ["https://yts.am/assets/images/movies/avengers_infinity_war_2018/large-cover.jpg", "https://yts.am/assets/images/movies/black_panther_2018/large-cover.jpg", "https://yts.am/assets/images/movies/deadpool_2_2018/large-cover.jpg", "https://yts.am/assets/images/movies/big_hero_6_2014/large-cover.jpg"]
+
+const movies = [
+  {
+    title: "Avengers",
+    image: "https://yts.am/assets/images/movies/avengers_infinity_war_2018/large-cover.jpg"
+  }, {
+    title: "Black Panther",
+    image: "https://yts.am/assets/images/movies/black_panther_2018/large-cover.jpg"
+  }, {
+    title: "Deadpool 2",
+    image: "https://yts.am/assets/images/movies/deadpool_2_2018/large-cover.jpg"
+  }, {
+    title: "Big Hero 6",
+    image: "https://yts.am/assets/images/movies/big_hero_6_2014/large-cover.jpg"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movie_title[0]} poster={movie_images[0]} />
-        <Movie title={movie_title[1]} poster={movie_images[1]} />
-        <Movie title={movie_title[2]} poster={movie_images[2]} />
-        <Movie title={movie_title[3]} poster={movie_images[3]} />
+        {movies.map((movie, index) => 
+           <Movie 
+            title={movie.title}
+            poster={movie.image}
+            key={index}
+          />
+        )}
       </div>
     )
   }
